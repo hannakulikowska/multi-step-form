72ac4e6f32b263f841e95d0f6009b05a128e1e1c
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-07-22 14:42:41 +0200

    feat: add background color, main title and form description

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import './App.scss';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Join our Community of Developers</h1>
+      <p>
+        To join our community and participate in frontend challenges. Please fill out the following
+        form.
+      </p>
+    </>
+  );
 }
 
 export default App;
