8e169e522f18fcab9eb7ceb0db9b60a25a97bb32
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-07-25 15:01:04 +0200

    feat: add form controls for the 1st step

--- src/components/steps/Step1.tsx
+++ src/components/steps/Step1.tsx
@@ -1,3 +1,4 @@
+import { Form } from 'react-bootstrap';
 import StepsContainer from '../StepsContainer';
 
 const Step1 = () => {
@@ -5,7 +6,22 @@ const Step1 = () => {
     <StepsContainer
       title="Personal Information"
       description="Please provide your personal details so we can get to know you better.">
-      <div>Step 1</div>
+      <Form.Group className="col-md-6 mb-3" controlId="formFullName">
+        <Form.Label>Full Name</Form.Label>
+        <Form.Control type="text" placeholder="John Doe" />
+      </Form.Group>
+      <Form.Group className="col-md-6 mb-3" controlId="formEmail">
+        <Form.Label>Email Address</Form.Label>
+        <Form.Control type="email" placeholder="name@email.com" />
+      </Form.Group>
+      <Form.Group className="col-md-6 mb-3" controlId="formPhone">
+        <Form.Label>Phone Number</Form.Label>
+        <Form.Control type="tel" placeholder="+91 1234567890" />
+      </Form.Group>
+      <Form.Group className="col-md-6 mb-3" controlId="formPortfolioLink">
+        <Form.Label>Portfolio/GitHub Link</Form.Label>
+        <Form.Control type="text" placeholder="github.com/johndoe" />
+      </Form.Group>
     </StepsContainer>
   );
 };
